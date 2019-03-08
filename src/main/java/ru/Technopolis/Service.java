ceaf3252ee3012a7c954beb7a71ff8dfd7f1e927
rusyamurzin <scretch235@mail.ru>
2019-03-08 16:30:46 +0300

    #1 HomeWork implementation

--- src/main/java/ru/Technopolis/Service.java
+++ src/main/java/ru/Technopolis/Service.java
@@ -1,5 +1,9 @@
 package ru.Technopolis;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import ru.Technopolis.model.ToDoDAO;
 @Controller
 public class Service {
 
+    private List<ToDo> listToDo = new ArrayList<>();
     private ToDoDAO dao;
 
     @RequestMapping("/")
@@ -27,6 +32,43 @@ public class Service {
     @RequestMapping( value = "/create", method = RequestMethod.POST)
     public @ResponseBody /*Превращает в JSON*/
     ToDo create(@RequestParam String description){
-        return dao.create(description);
+        ToDo response = dao.create(description);
+        listToDo.add(response);
+        return response;
+    }
+
+    @RequestMapping( value = "/delete", method = RequestMethod.DELETE)
+    public @ResponseBody /*Превращает в JSON*/
+    ToDo delete(@RequestParam long id){
+        ToDo response = null;
+        Iterator it = listToDo.iterator();
+        while(it.hasNext()){
+            ToDo nextValue = (ToDo)it.next();
+            if(nextValue.getId() == id) {
+                response = nextValue;
+                it.remove();
+            }
+        }
+        return response;
+    }
+
+    @RequestMapping( value = "/read", method = RequestMethod.GET)
+    public @ResponseBody /*Превращает в JSON*/
+    ToDo [] read(){
+        return listToDo.toArray(new ToDo[listToDo.size()]);
+    }
+
+    @RequestMapping( value = "/update", method = RequestMethod.POST)
+    public @ResponseBody /*Превращает в JSON*/
+    ToDo update(@RequestParam long id, String description){
+        ToDo response = new ToDo(id, description);
+        Iterator it = listToDo.iterator();
+        while(it.hasNext()){
+            ToDo nextValue = (ToDo)it.next();
+            if(nextValue.getId() == id) {
+                listToDo.set(listToDo.indexOf(nextValue), response);
+            }
+        }
+        return response;
     }
 }
