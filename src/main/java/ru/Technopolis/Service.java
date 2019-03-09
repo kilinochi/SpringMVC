7d087d474841b5ad153eb882814f2cfaa2603282
Daniil <danilp2505@gmail.com>
2019-03-09 14:55:51 +0300

    added read, update, delete mapping

--- src/main/java/ru/Technopolis/Service.java
+++ src/main/java/ru/Technopolis/Service.java
@@ -9,24 +9,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import ru.Technopolis.model.ToDo;
 import ru.Technopolis.model.ToDoDAO;
 
+import java.util.List;
+
 @Controller
 public class Service {
 
     private ToDoDAO dao;
+
+    @Autowired
+    public Service(ToDoDAO dao) {
+        this.dao = dao;
+    }
 
     @RequestMapping("/")
     public String index() {
         return "index";
     }
 
-    @Autowired //Dependency Injection
-    public Service(ToDoDAO dao){
-        this.dao = dao;
-    }
-
-    @RequestMapping( value = "/create", method = RequestMethod.POST)
-    public @ResponseBody /*Превращает в JSON*/
-    ToDo create(@RequestParam String description){
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    @ResponseBody
+    public ToDo create(@RequestParam String description) {
         return dao.create(description);
     }
+
+    @RequestMapping(value = "/read", method = RequestMethod.GET)
+    @ResponseBody
+    public List<ToDo> read() {
+        return dao.getToDoList();
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.GET)
+    @ResponseBody
+    public ToDo update(@RequestParam long id, @RequestParam String description) {
+        return dao.update(id, description);
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    @ResponseBody
+    public ToDo delete(@RequestParam long id) {
+        return dao.delete(id);
+    }
+
+
 }
--- src/main/java/ru/Technopolis/model/ToDoDAO.java
+++ src/main/java/ru/Technopolis/model/ToDoDAO.java
@@ -2,14 +2,35 @@ package ru.Technopolis.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
-@Component /*Кладем в контейнер */
+@Component
 public class ToDoDAO {
-    private static AtomicLong counter = new AtomicLong();
 
-    public ToDo create(String description){
+    private static AtomicLong counter = new AtomicLong();
+    private Map<Long, ToDo> toDoMap = new LinkedHashMap<>();
+
+    public ToDo create(String description) {
         long id = counter.incrementAndGet();
-        return new ToDo(id,description);
+        ToDo toDo = new ToDo(id, description);
+        toDoMap.put(id, toDo);
+        return toDo;
+    }
+
+    public List<ToDo> getToDoList() {
+        return new ArrayList<>(toDoMap.values());
+    }
+
+    public ToDo update(long id, String description) {
+        return toDoMap.replace(id, new ToDo(id, description));
+    }
+
+    public ToDo delete(long id) {
+        return toDoMap.remove(id);
     }
 }
