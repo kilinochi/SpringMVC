c782f32b980e166bb4956f72f473f7d9ca19dcc9
Denispok <shakhover@yandex.ru>
2019-03-11 22:41:28 +0300

    Basic CRUD

--- src/main/java/ru/Technopolis/Service.java
+++ src/main/java/ru/Technopolis/Service.java
@@ -2,10 +2,14 @@ package ru.Technopolis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import ru.Technopolis.model.ToDo;
 import ru.Technopolis.model.ToDoDAO;
 
@@ -20,13 +24,30 @@ public class Service {
     }
 
     @Autowired //Dependency Injection
-    public Service(ToDoDAO dao){
+    public Service(ToDoDAO dao) {
         this.dao = dao;
     }
 
-    @RequestMapping( value = "/create", method = RequestMethod.POST)
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
     public @ResponseBody /*Превращает в JSON*/
-    ToDo create(@RequestParam String description){
+    ToDo create(@RequestParam String description) {
         return dao.create(description);
+    }
+
+    @GetMapping(value = "/read")
+    public @ResponseBody
+    ToDo read(@RequestParam long id) {
+        return dao.read(id);
+    }
+
+    @PatchMapping(value = "/update")
+    public @ResponseBody
+    ToDo update(@RequestParam long id, @RequestParam String description) {
+        return dao.update(id, description);
+    }
+
+    @DeleteMapping(value = "/delete")
+    public void delete(@RequestParam long id) {
+        dao.delete(id);
     }
 }
--- src/main/java/ru/Technopolis/model/ToDoDAO.java
+++ src/main/java/ru/Technopolis/model/ToDoDAO.java
@@ -1,15 +1,36 @@
 package ru.Technopolis.model;
 
-import org.springframework.stereotype.Component;
-
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Component;
 
 @Component /*Кладем в контейнер */
 public class ToDoDAO {
     private static AtomicLong counter = new AtomicLong();
+    private final HashMap<Long, ToDo> toDos = new HashMap<>();
 
-    public ToDo create(String description){
+    public ToDo create(String description) {
         long id = counter.incrementAndGet();
-        return new ToDo(id,description);
+        ToDo toDo = new ToDo(id, description);
+        toDos.put(toDo.getId(), toDo);
+        return toDo;
+    }
+
+    public ToDo read(long id) {
+        return toDos.get(id);
+    }
+
+    public ToDo update(long id, String description) {
+        ToDo toDo = toDos.get(id);
+        if (toDo != null) {
+            toDo.setDescription(description);
+            return toDo;
+        }
+        return null;
+    }
+
+    public void delete(long id) {
+        toDos.remove(id);
     }
 }
