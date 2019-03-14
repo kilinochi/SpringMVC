89ea6d441ac0e25f3173774a7a03407944cb26f8
Yaroslav Erokhin <yaroslav98er@gmail.com>
2019-03-14 13:45:22 +0300

    Task 1

--- src/main/java/ru/Technopolis/Service.java
+++ src/main/java/ru/Technopolis/Service.java
@@ -9,10 +9,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import ru.Technopolis.model.ToDo;
 import ru.Technopolis.model.ToDoDAO;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 @Controller
 public class Service {
 
     private ToDoDAO dao;
+    private List<ToDo> ToDoList = new ArrayList<>();
 
     @RequestMapping("/")
     public String index() {
@@ -24,9 +30,62 @@ public class Service {
         this.dao = dao;
     }
 
-    @RequestMapping( value = "/create", method = RequestMethod.POST)
+    @RequestMapping( value = "/create", method = {RequestMethod.POST, RequestMethod.GET})
     public @ResponseBody /*Превращает в JSON*/
-    ToDo create(@RequestParam String description){
-        return dao.create(description);
+    boolean create(@RequestParam String description) {
+        try {
+            ToDoList.add(dao.create(description));
+        }
+        catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @RequestMapping(value = "/update", method = {RequestMethod.POST, RequestMethod.GET})
+    public @ResponseBody
+    boolean update(@RequestParam long id, @RequestParam String description) {
+        ListIterator it = ToDoList.listIterator();
+        while (it.hasNext()) {
+            if (((ToDo)it.next()).getId() == id) {
+                it.set(dao.create(description));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @RequestMapping(value = "/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public @ResponseBody
+    boolean delete(@RequestParam long id) {
+        Iterator it = ToDoList.iterator();
+        while (it.hasNext()) {
+            if (((ToDo)it.next()).getId() == id) {
+                it.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @RequestMapping(value = "/deleteDesc", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public @ResponseBody
+    boolean delete(@RequestParam String description) {
+        Iterator it = ToDoList.iterator();
+        while (it.hasNext()) {
+            if (((ToDo)it.next()).getDescription().equals(description)) {
+                it.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public @ResponseBody
+    ToDo[] get() {
+        return ToDoList.toArray(new ToDo[ToDoList.size()]);
     }
 }
--- src/main/java/ru/Technopolis/model/ToDo.java
+++ src/main/java/ru/Technopolis/model/ToDo.java
@@ -18,4 +18,5 @@ public class ToDo {
     public String getDescription() {
         return description;
     }
+
 }
