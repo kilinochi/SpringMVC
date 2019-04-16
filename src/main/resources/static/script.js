c8ae552b9f06d2cf7dbd6a057a03b4c4da3139af
Nadya <spinakernadya7@gmail.com>
2019-04-17 00:02:06 +0300

    def

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,106 @@
+function CreateRequest() {
+    var Request = false;
+    if (window.XMLHttpRequest) Request = new XMLHttpRequest();
+    else if (window.ActiveXObject)
+    {
+        try { Request = new ActiveXObject("Microsoft.XMLHTTP");}
+        catch (CatchException) {Request = new ActiveXObject("Msxml2.XMLHTTP");}
+    }
+    if (!Request) alert("Невозможно создать XMLHttpRequest");
+    return Request;
+}
+
+function SendRequest(r_method, r_path, r_args, r_handler) {
+    var Request = CreateRequest();
+    if (!Request) return;
+    Request.onreadystatechange = function()
+    {
+        //Если обмен данными завершен
+        if (Request.readyState == 4)
+        {
+            //Передаем управление обработчику пользователя
+            //r_handler(Request);
+        }
+    }
+    //Проверяем, если требуется сделать GET-запрос
+    if (r_method.toLowerCase() == "get" && r_args.length > 0)
+        r_path += "?" + r_args;
+    //Инициализируем соединение
+    Request.open(r_method, r_path, true);
+    if (r_method.toLowerCase() == "post")
+    {
+        //Если это POST-запрос
+        //Устанавливаем заголовок
+        Request.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=utf-8");
+        //Посылаем запрос
+        Request.send(r_args);
+    }
+    else
+    {
+        //Если это GET-запрос
+        //Посылаем нуль-запрос
+        Request.send(null);
+    }
+}
+function CreateProdById(str) {
+    var data = new FormData();
+    data.append("str", str);
+    SendRequest("GET","/create", data, creteRequestHandler());
+    var prot = document.getElementById(id);
+    prot.parentElement.removeChild(prot);
+}
+
+function UpdateProdById(id,str) {
+    var data = new FormData();
+    data.append("id", id);
+    SendRequest("GET","/update", data, updateRequestHandler());
+    var prot = document.getElementById(id);
+    prot.parentElement.removeChild(prot);
+}
+
+function NameProdById(id) {
+    var data = new FormData();
+    data.append("id", id);
+    SendRequest("GET","/name", data, nameRequestHandler());
+    var prod = document.getElementById(id);
+    prod.parentElement.removeChild(prod);
+}
+
+function DeleteProdById(id) {
+    var data = new FormData();
+    data.append("id", id);
+    SendRequest("GET","/delete", data, deletionRequestHandler());
+    var prod = document.getElementById(id);
+    prod.parentElement.removeChild(prod);
+}
+
+function listProdById() {
+    SendRequest("GET","/list", "", listRequestHandler());
+    var prod = document.getElementById(id);
+    prod.parentElement.removeChild(prod);
+}
+function DeleteProd(id) {
+    console.log("Delete Prod %d",id);
+
+    var data = new FormData();
+    data.append("id", id);
+    id="id="+id;
+    SendRequest("GET","/delete",id, "");
+    location.reload ();
+}
+
+
+function CreateProd() {
+    var nodes = document.querySelector(".todo-creator_text-input");
+    var str = nodes.value;
+    if (str.trim() === "") {alert("Task text is empty!");return;}
+    nodes.value = "";
+    console.log("Create %s", str);
+    str="desc="+str;
+    SendRequest("GET","/create",str, "");
+    location.reload ();
+}
+
 function ShowAll() {
     var nodes = document.querySelectorAll(".todos-list_item");
     var items = Array.from(nodes);
